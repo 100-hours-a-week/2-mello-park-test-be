ae7d29ca2e2f2d2285f61fa88d9fe673a6bcaa1d
lunghyun <nangmannsh@gmail.com>
2025-02-06 23:50:56 +0900

    [UPDATE] post 요청 생성: /users, {name, email}

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const app = express();
 // 웹 서버가 사용할 포트 번호를 정의합니다.
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 /**
 * 루트 경로('/')에 대한 GET 요청을 처리
 * 요청이 오면 'Hello World!' 문자열을 응답
@@ -19,6 +22,13 @@ app.get('/', (request, response) => {
     return response.send('Hello World!');
 });
 
+app.post('/users', (request, response) => {
+    const name = request.body.name;
+    const email = request.body.email;
+
+    return response.send(`${name}, ${email}`);
+});
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
